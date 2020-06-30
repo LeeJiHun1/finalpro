7f89dc21ccf8f258a4580733d1bd210603ed45ee
Philip Kim <48585494+woosukplay3@users.noreply.github.com>
2020-06-30 17:01:44 +0900

    (수정)

--- app/src/main/java/com/example/finalpro/main/MainPage.kt
+++ app/src/main/java/com/example/finalpro/main/MainPage.kt
@@ -2,10 +2,39 @@ package com.example.finalpro.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main_page.*
 
 class MainPage : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
+
+        bottomNav.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_home -> {
+                    setContent("Home")
+                    true
+                }
+                R.id.menu_notification -> {
+                    setContent("Notification")
+                    true
+                }
+                R.id.menu_search -> {
+                    setContent("Search")
+                    true
+                }
+                R.id.menu_profile -> {
+                    setContent("Profile")
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun setContent(content: String) {
+        setTitle(content)
+        tvLabel.text = content
     }
 }
